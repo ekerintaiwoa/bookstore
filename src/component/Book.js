277897f9b46f618777a52956a4634461f818cfb5
Taiwo Ekerin <83164200+ekerintaiwoa@users.noreply.github.com>
2022-07-20 19:25:42 +0200

    created  Book component

--- src/component/Book.js
+++ src/component/Book.js
@@ -1,9 +1,8 @@
-import store from "../redux/Store";
-import  {ordered,restocked} from '../redux/features/BookSlice'
+
 
 //import action we want to dispatch
 
-
+/*
 
 console.log('initialstae', store.getState());
 
@@ -13,3 +12,37 @@ const unsubscribe =  store.subscribe(()=>{
   console.log("updated state",store.getState())
 
 })
+
+*/
+
+import React, {useState} from 'react';
+import {useDispatch , useSelector} from "react-redux";
+
+const Book = () => {
+
+    let dispatch = useDispatch();
+
+    // get data from Redux Store
+
+    let bookState = useSelector((state) => {
+        return state["numberOfBooks"]; 
+    });
+
+    let {bookNumber} = bookState;
+
+
+
+    return ( 
+
+            <div>
+
+                    {bookNumber}
+
+            </div>
+
+
+
+     );
+}
+ 
+export default Book;
